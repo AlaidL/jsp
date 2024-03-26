de1c248c93b2126623546b073d563f3e9f5ace41
Alaid Leon <43395976+AlaidL@users.noreply.github.com>
2024-03-26 00:06:39 -0600

    12

--- jasmin/managers/clients.py
+++ jasmin/managers/clients.py
@@ -615,8 +615,7 @@ class SMPPClientManagerPB(pb.Avatar):
                 self.redisClient.hmset(hashKey, hashValues).addCallback(
                     lambda response: self.redisClient.expire(
                         hashKey, connector['config'].dlr_expiry))
-        elif (isinstance(source_connector, SMPPServerProtocol) and
-              SubmitSmPDU.params['registered_delivery'].receipt != RegisteredDeliveryReceipt.NO_SMSC_DELIVERY_RECEIPT_REQUESTED):
+        elif (isinstance(source_connector, SMPPServerProtocol)):
             # If submit_sm is successfully sent from a SMPPServerProtocol connector and DLR is
             # requested, then map message-id to the source_connector to permit related deliver_sm
             # messages holding further receipts to be sent back to the right connector
