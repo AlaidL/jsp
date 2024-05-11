347b0bd6eb2c71de1ca0e9ddd9bd1d182c460685
Alaid Leon <43395976+AlaidL@users.noreply.github.com>
2024-05-11 12:42:55 -0600

    632

--- jasmin/protocols/rest/config.py
+++ jasmin/protocols/rest/config.py
@@ -26,7 +26,7 @@ enable_utc = True
 # Will push messages back to Jasmin's http api with a maximum throughput *per worker*
 # Please consider this is a *per worker* throughput !
 # Set to zero (0) to disable throughput control
-http_throughput_per_worker = 8
+http_throughput_per_worker = 15
 # When set to True, the batch executor will consider Jasmin's response time to
 # control the batch throughput, slower response time will slow down the throughput
 # and vice-versa
